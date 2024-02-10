23937b1eea4802e7174ab4ae9a627e53b9b49661
Dan <ulturgashev112@gmail.com>
2024-02-11 00:30:45 +0300

    settings fix

--- src/settings.py
+++ src/settings.py
@@ -1,20 +1,20 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    postgres_host: str
-    postgres_port: str
-    postgres_user: str
-    postgres_password: str
-    postgres_database_name: str
+    postgres_host: str = "localhost"
+    postgres_port: str = "5432"
+    postgres_user: str = "postgres"
+    postgres_password: str = "postgres"
+    postgres_database_name: str = "postgres"
 
-    secret: str
-    algorithm: str
-    access_token_expire_minutes: int
+    secret: str = "secret"
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 5
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    model_config = SettingsConfigDict(
+        env_file=(".env", ".env.prod"), env_file_encoding="utf-8"
+    )
 
 
 settings = Settings()
